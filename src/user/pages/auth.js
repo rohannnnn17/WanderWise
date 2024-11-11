d32b83496ed9ca081138d3dbf67f2f5286685061
Rohan <pawar17rohan@gmail.com>
2024-11-11 10:28:47 +0530

    Updated Front end
    fixed a bug where clicking sign up loggs user in

--- src/user/pages/auth.js
+++ src/user/pages/auth.js
@@ -1,26 +1,28 @@
 import React, { useState, useContext } from "react";
-import styled from "styled-components";
-import Button from "../../shared/components/FormElements/Button";
-import { useForm } from "../../shared/hooks/form-hook";
+
+import Card from "../../shared/components/UIElements/Card";
 import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from "../../shared/util/validators";
-import "../../shared/context/auth-context";
+import { useForm } from "../../shared/hooks/form-hook";
 import { AuthContext } from "../../shared/context/auth-context";
+import "./auth.css";
 
-const Authenticate = () => {
+const Auth = () => {
   const auth = useContext(AuthContext);
   const [isLoginMode, setIsLoginMode] = useState(true);
+
   const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
         value: "",
         isValid: false,
       },
-      pwd: {
+      password: {
         value: "",
         isValid: false,
       },
@@ -28,15 +30,16 @@ const Authenticate = () => {
     false
   );
 
-  const authSubmitHandler = (event) => {
-    event.preventDefault();
-    // Handle form submission here
-    console.log(formState.inputs); // This will give you the current form state
-    auth.login();
-  };
-
-  const switchModeHandler = (event) => {
-    if (isLoginMode) {
+  const switchModeHandler = () => {
+    if (!isLoginMode) {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: undefined,
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
       setFormData(
         {
           ...formState.inputs,
@@ -47,22 +50,20 @@ const Authenticate = () => {
         },
         false
       );
-    } else {
-      setFormData(
-        {
-          email: formState.inputs.email,
-          pwd: formState.inputs.pwd,
-        },
-        formState.inputs.email.isValid && formState.inputs.pwd.isValid
-      );
     }
-
     setIsLoginMode((prevMode) => !prevMode);
   };
 
+  const authSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+    auth.login();
+  };
+
   return (
-    <AuthContainer>
-      <AuthHeader>{isLoginMode ? "Login" : "Sign Up"}</AuthHeader>
+    <Card className="authentication">
+      <h2>Login Required</h2>
+      <hr />
       <form onSubmit={authSubmitHandler}>
         {!isLoginMode && (
           <Input
@@ -71,60 +72,37 @@ const Authenticate = () => {
             type="text"
             label="Your Name"
             validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter your name."
+            errorText="Please enter a name."
             onInput={inputHandler}
           />
         )}
         <Input
-          id="email"
           element="input"
+          id="email"
           type="email"
-          label="Email"
-          validators={[VALIDATOR_EMAIL(), VALIDATOR_REQUIRE()]}
+          label="E-Mail"
+          validators={[VALIDATOR_EMAIL()]}
           errorText="Please enter a valid email address."
           onInput={inputHandler}
         />
         <Input
-          id="pwd"
           element="input"
+          id="password"
           type="password"
           label="Password"
           validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Please enter a valid password."
+          errorText="Please enter a valid password, at least 5 characters."
           onInput={inputHandler}
         />
         <Button type="submit" disabled={!formState.isValid}>
-          {isLoginMode ? "LOGIN" : "SIGN UP"}
-        </Button>
-        <Button inverse onClick={switchModeHandler}>
-          SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
+          {isLoginMode ? "LOGIN" : "SIGNUP"}
         </Button>
       </form>
-    </AuthContainer>
+      <Button inverse onClick={switchModeHandler}>
+        SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
+      </Button>
+    </Card>
   );
 };
 
-export default Authenticate;
-
-// Styled Components
-const AuthContainer = styled.div`
-  width: 90%;
-  max-width: 25rem;
-  margin: 7rem auto;
-  text-align: center;
-  background-color: #f4f4f4;
-  padding: 2rem;
-  border-radius: 10px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-
-  @media (max-width: 768px) {
-    margin: 5rem auto;
-  }
-`;
-
-const AuthHeader = styled.h2`
-  color: white;
-  text-align: center;
-  font-size: 1.5rem;
-  margin-bottom: 1rem;
-`;
+export default Auth;
